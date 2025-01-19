f4c79979df9cc7e8abbb565f20641c0d4f011f59
Md. Maruf Hossain <marufhossain123098@gmail.com>
2025-01-19 23:21:58 +0600

    Update dbCon.js

--- server/database/dbCon.js
+++ server/database/dbCon.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbCn = async () => {
   try {
-    await mongoose.connect("https://url-shortner-1-agxz.onrender.com/url_collection");
+    await mongoose.connect("mongodb://localhost:27017/url_collection");
     console.log("database connected successfully");
   } catch (error) {
     console.log(error);
