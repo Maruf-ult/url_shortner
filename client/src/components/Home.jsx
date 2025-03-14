8bd307f828b5b04742cc1fd89f4c17d4280fad64
Md. Maruf Hossain <marufhossain123098@gmail.com>
2025-03-14 16:11:10 +0600

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -98,7 +98,8 @@ function Home() {
 
 
   return (
-    <>
+    
+      <>
       <div className="h-screen w-screen bg-emerald-200 relative">
         <div className="flex-col justify-start absolute space-y-5 mt-20 ml-24">
           <h1 className="font-extrabold text-8xl">
@@ -112,12 +113,12 @@ function Home() {
 
         <div className="flex justify-end">
           {view === 'card' ? (
-            <div className="flex-col justify-start text-start bg-slate-200 my-20 mr-36 h-96 w-96 space-y-4 pt-6 rounded-md pl-4 shadow-2xl">
-              <div className="space-y-2 border-solid border-black">
-                <p className="mr-28 ml-1">your long url</p>
+            <div className="flex-col justify-start text-start bg-slate-200 my-16 py-20  mr-10 h-auto w-[600px] space-y-8 pt-8  rounded-md pl-6 shadow-2xl">
+              <div className="space-y-3 border-solid border-black pr-5">
+                <p className="ml-2 text-xl">Your long URL</p>
                 <input
                   onChange={handleChange}
-                  className="bg-white border-black h-10 px-2 w-80 rounded"
+                  className="bg-white border-black h-16 px-6 w-full rounded-md text-lg "
                   type="url"
                   name="long_url"
                   placeholder="Enter long link here"
@@ -126,74 +127,75 @@ function Home() {
                 />
               </div>
 
-              <div className="space-y-2">
-                <p className="mr-40 ml-2">tiny_url</p>
-                <p className="border-black bg-white h-10 px-2 mr-9 pt-1 rounded">
+              <div className="space-y-3 pr-5">
+                <p className="ml-2 text-xl">Tiny URL</p>
+                <p className="border-black bg-white h-16 px-4 pt-1 rounded-md text-lg">
                   {data ? data : ""}
                 </p>
               </div>
 
-              <div className="flex justify-start space-x-4 pt-3">
+              <div className="flex justify-start space-x-4 pt-4">
                 <button
                   onClick={clearUrl}
-                  className="bg-white border-blue-400 border-2 px-5 py-1 rounded-md"
+                  className="bg-white border-blue-400 border-2 px-6 py-2 rounded-md text-lg"
                   disabled={isDisabled}
                 >
-                  clear
+                  Clear
                 </button>
                 <button
                   onClick={opa2}
-                  className="bg-blue-500 px-6 py-1 rounded-md"
+                  className="bg-blue-500 px-8 py-2 rounded-md text-lg"
                   disabled={isDisabled}
                 >
                   QR
                 </button>
                 <button onClick={link}
-                  className="bg-blue-500 px-4 py-1 rounded-md"
+                  className="bg-blue-500 px-6 py-2 rounded-md text-lg"
                   disabled={isDisabled}
                 >
-                  share
+                  Share
                 </button>
                 <button
                   onClick={copyUrl}
-                  className="bg-green-500 px-4 py-1 rounded-md"
+                  className="bg-green-500 px-6 py-2 rounded-md text-lg"
                   disabled={isDisabled}
                 >
-                  copy
+                  Copy
                 </button>
               </div>
 
-              <div className="flex justify-start space-x-3 pt-3">
+              <div className="flex justify-start space-x-4 pt-4">
                 <button
                   onClick={nav1}
-                  className="bg-white border-solid border-green-500 px-14 py-3 border-2 rounded-md"
+                  className="bg-white border-solid border-green-500 px-16 py-4 border-2 rounded-md text-lg"
                   disabled={isDisabled}
                 >
-                  my_url
+                  My URLs
                 </button>
                 <button
                   onClick={createPost}
-                  className="bg-green-600 border-solid text-white px-14 py-3 rounded-md"
+                  className="bg-green-600 text-white px-16 py-4 rounded-md text-lg"
                   disabled={isDisabled}
                 >
-                  short_url
+                  Short URL
                 </button>
               </div>
             </div>
           ) : (
-            <div className="flex-col justify-start text-start bg-slate-200 my-20 mr-36 h-96 w-96 space-y-14 pt-4  rounded-md pl-4 shadow-2xl">
-            <button
-              onClick={backToCard}
-              className="flex justify-end bg-blue-500 px-8 py-1 rounded-md mx-64  "
-            >
-              Back
-            </button>
-            <img src={qr} alt="QR Code" className="mx-auto" />
-          </div>
+            <div className="flex-col justify-start text-start bg-slate-200 my-20 mr-36 h-auto w-[600px] space-y-8 pt-6 rounded-md pl-6 shadow-2xl">
+              <button
+                onClick={backToCard}
+                className="flex justify-end bg-blue-500 px-10 py-2 rounded-md mx-64"
+              >
+                Back
+              </button>
+              <img src={qr} alt="QR Code" className="mx-auto" />
+            </div>
           )}
         </div>
       </div>
       <Toaster />
+    
     </>
   );
 }
