564aee8e394ba3583e562d0a1e67cbc1821a85ac
Md. Maruf Hossain <marufhossain123098@gmail.com>
2025-03-14 15:37:42 +0600

    Update dbCon.js

--- server/database/dbCon.js
+++ server/database/dbCon.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbCn = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/url_collection");
+      await mongoose.connect("mongodb+srv://marufapurbo911:34oZdfCBMCOvknMP@cluster0.v5vkx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("database connected successfully");
   } catch (error) {
     console.log(error);
